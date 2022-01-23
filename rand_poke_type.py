8183b3ca60a57dd074c2b01f82c70a08b2e7eb1a
Matti Gilmore <mattigilmore89@gmail.com>
2022-01-23 02:34:43 -0500

    Add files via upload

---
+++ rand_poke_type.py
@@ -0,0 +1,4 @@
+import random
+#this program randomly choosing a pokemon type
+pokemon_type=random.choice(['Normal','Fire','Water','Grass','Electric','Ice','Fighting','Poison','Ground','Flying','Psychic','Bug','Rock','Ghost','Dark','Dragon','Steel','Fairy'])
+print(pokemon_type)
